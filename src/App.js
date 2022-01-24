a3e18917b239f8c44401c8f4709972347eaafc4b
Piyush <piyushat25@gmail.com>
2022-01-25 03:09:34 +0530

    added cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Cards />
       <Footer />
     </div>
   );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import example from './1.png'
 function Cards() {
     return <div className='card'>
-        <h3>Meme Generator</h3>
-        <img src={example} className='card--img' alt="" />
+        <div>
+            <h3>Meme Generator</h3>
+            <img src={example} className='card--img' alt="" />
+        </div>
+        <div>
+            <h3>Meme Generator 33334</h3>
+            <img src={example} className='card--img' alt="" />
+        </div>
     </div>;
 }
 
